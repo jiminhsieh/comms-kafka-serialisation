350c7ddbda794032800cddc6a628d2e309f7047b
Chris Birchall <chris.birchall@gmail.com>
2017-05-12 09:50:03 +0100

    Stop using circe for deserialisation (#4)
    * Stop using circe for deserialisation
    
    Go back to using avro4s for deserialisation as well as serialisation
    
    The only reason we were using circe was so we could add new fields to
    schemas. But when we move to the schema registry this shouldn't be a
    problem any more.
    
    * Use our fork of avro4s

--- src/main/scala/com/ovoenergy/comms/serialisation/Codecs.scala
+++ src/main/scala/com/ovoenergy/comms/serialisation/Codecs.scala
@@ -2,122 +2,10 @@ package com.ovoenergy.comms.serialisation
 
 import java.time.Instant
 
-import com.sksamuel.avro4s.{ToSchema, ToValue}
-import io.circe._
-import io.circe.optics._
+import com.sksamuel.avro4s.{FromValue, ToSchema, ToValue}
 import org.apache.avro.{LogicalTypes, Schema}
-import shapeless._
-import shapeless.labelled._
 
-trait DecoderUtil {
-
-  /**
-    * Avro unions are encoded as a JSON object with a single field,
-    * whose key is a type hint and whose value is the JSON encoding of the actual data.
-    * e.g. `{ "string": "foo" }`.
-    *
-    * We ignore the type hint and descend into the nested value.
-    */
-  protected def getTheOnlyFieldOfJsonObject(json: Json): Option[Json] =
-    JsonPath.root.each.json.headOption(json)
-
-  implicit final val decodeCNil: Decoder[CNil] = new Decoder[CNil] {
-    def apply(c: HCursor): Decoder.Result[CNil] = Left(DecodingFailure("CNil", c.history))
-  }
-
-}
-
-trait LowPriorityDecoders extends DecoderUtil {
-
-  trait CoproductDecoder[A] {
-    def decoder: Decoder[A]
-  }
-
-  implicit final val coproductDecoderCNil: CoproductDecoder[CNil] = new CoproductDecoder[CNil] {
-    val decoder = decodeCNil
-  }
-
-  implicit final def coproductDecoderCCons[L, R <: Coproduct](implicit
-                                                              decodeL: Decoder[L],
-                                                              decodeR: Lazy[CoproductDecoder[R]]
-                                                             ): CoproductDecoder[L :+: R] = new CoproductDecoder[L :+: R] {
-    val decoder: Decoder[L :+: R] = decodeL.map(Inl(_)).or(decodeR.value.decoder.map(Inr(_)))
-  }
-
-  implicit final def decodeCCons[L, R <: Coproduct](implicit
-                                                    coproductDecoder: CoproductDecoder[L :+: R]
-                                                   ): Decoder[L :+: R] = Decoder.instance[L :+: R] { (c: HCursor) =>
-    getTheOnlyFieldOfJsonObject(c.value) match {
-      case Some(json) => coproductDecoder.decoder.decodeJson(json)
-      case None => Left(DecodingFailure(s"Error decoding Coproduct value", c.history))
-    }
-  }
-
-}
-
-object Codecs extends LowPriorityDecoders {
-
-  implicit def decodeOption[T : Decoder]: Decoder[Option[T]] = Decoder.withReattempt[Option[T]] { (c: ACursor) =>
-    /*
-    An optional field could be None in two different ways:
-
-    1. The field is present and explicitly set to null.
-       This is how Avro encodes optional fields.
-
-    2. The field is not present at all in the JSON,
-       e.g. because the producer is using an older schema version that doesn't include the field
-     */
-    c.focus match {
-      case Some(value) =>
-        if (value.isNull) {
-          Right(None)
-        } else {
-          getTheOnlyFieldOfJsonObject(value) match {
-            case Some(json) => json.as[T].right.map(Some(_))
-            case None => Left(DecodingFailure(s"Error decoding option value", c.history))
-          }
-        }
-      case None =>
-        Right(None)
-    }
-  }
-
-  trait EnumDecoder[A] {
-    def decoder: Decoder[A]
-  }
-
-  implicit final val enumDecoderCNil: EnumDecoder[CNil] = new EnumDecoder[CNil] {
-    val decoder = decodeCNil
-  }
-
-  implicit def enumDecoderCCons[K <: Symbol, V, R <: Coproduct](implicit
-                                                                wit: Witness.Aux[K],
-                                                                emptyGen: LabelledGeneric.Aux[V, HNil],
-                                                                edr: EnumDecoder[R]
-                                                               ): EnumDecoder[FieldType[K, V] :+: R] =
-    new EnumDecoder[FieldType[K, V] :+: R] {
-      val decoder = Decoder.instance[FieldType[K, V] :+: R](c =>
-        c.as[String] match {
-          case Right(s) if s == wit.value.name => Right(Inl(field[K](emptyGen.from(HNil))))
-          case Right(_) => edr.decoder.apply(c).right.map(Inr(_))
-          case Left(err) => Left(DecodingFailure(s"Failed to decode enum ($err)", c.history))
-        }
-      )
-    }
-
-  implicit def decodeEnum[A, R <: Coproduct](implicit
-                                             gen: LabelledGeneric.Aux[A, R],
-                                             edr: EnumDecoder[R]
-                                            ): Decoder[A] =
-    Decoder.instance[A](c => edr.decoder(c).right.map(gen.from))
-
-  /**
-    * Instant serialized to Long as per the 'Kafka Manifesto'
-    */
-  implicit def decodeInstant: Decoder[Instant] = Decoder.decodeLong.emap { long =>
-    import cats.syntax.either._
-    Either.catchNonFatal(Instant.ofEpochMilli(long)).leftMap(t => t.getMessage)
-  }
+object Codecs {
 
   implicit object InstantToSchema extends ToSchema[Instant] {
     override val schema: Schema = {
@@ -130,6 +18,9 @@ object Codecs extends LowPriorityDecoders {
     override def apply(value: Instant): Long = value.toEpochMilli
   }
 
+  implicit object InstantFromValue extends FromValue[Instant] {
+    override def apply(value: Any, field: Schema.Field) = Instant.ofEpochMilli(value.toString.toLong)
+  }
 
 
 }
--- src/main/scala/com/ovoenergy/comms/serialisation/Serialisation.scala
+++ src/main/scala/com/ovoenergy/comms/serialisation/Serialisation.scala
@@ -1,15 +1,12 @@
 package com.ovoenergy.comms.serialisation
 
-import java.io.ByteArrayOutputStream
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
 import java.nio.charset.StandardCharsets
 import java.util
 
 import com.sksamuel.avro4s._
 import org.apache.kafka.common.serialization.{Deserializer, Serializer}
 import org.slf4j.LoggerFactory
-import io.circe._
-import io.circe.parser._
-import cats.syntax.either._
 
 import scala.reflect.ClassTag
 
@@ -17,20 +14,21 @@ object Serialisation {
 
   val log = LoggerFactory.getLogger(getClass)
 
-  def avroDeserializer[T: Decoder: ClassTag]: Deserializer[Option[T]] =
+  def avroDeserializer[T: SchemaFor: FromRecord: ClassTag]: Deserializer[Option[T]] =
     new Deserializer[Option[T]] {
       override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {}
       override def close(): Unit = {}
       override def deserialize(topic: String, data: Array[Byte]): Option[T] = {
-        val string = new String(data, StandardCharsets.UTF_8)
-        val result = decode[T](string)
+        val bais = new ByteArrayInputStream(data)
+        val input = AvroJsonInputStream[T](bais)
+        val result = input.singleEntity
 
-        result.left.foreach { e =>
+        result.failed.foreach { e =>
           val className = implicitly[ClassTag[T]].runtimeClass.getSimpleName
           log.warn(
             s"""Skippping event because it could not be deserialized to $className
                |Event:
-               |$string
+               |${new String(data, StandardCharsets.UTF_8)}
             """.stripMargin,
             e)
         }
