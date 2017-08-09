a16aaac9ed9b0584f126e937e72e33a655faeba4
Laurence Bird <laurence.bird@ovoenergy.com>
2017-08-09 15:20:58 +0100

    Add more type class instances of HasCommName

--- modules/helpers/src/main/scala/com/ovoenergy/comms/helpers/HasCommName.scala
+++ modules/helpers/src/main/scala/com/ovoenergy/comms/helpers/HasCommName.scala
@@ -1,8 +1,8 @@
 package com.ovoenergy.comms.helpers
 
 import com.ovoenergy.comms.model._
-import com.ovoenergy.comms.model.email.OrchestratedEmailV3
-import com.ovoenergy.comms.model.sms.OrchestratedSMSV2
+import com.ovoenergy.comms.model.email.{EmailProgressedV2, OrchestratedEmailV3}
+import com.ovoenergy.comms.model.sms.{OrchestratedSMSV2, SMSProgressedV2}
 
 trait HasCommName[A] {
   def commName(a: A): String
@@ -13,11 +13,17 @@ object HasCommName {
     def commName(a: A): String = getCommName(a)
   }
 
-  implicit val failedCancellationHasCommName =
+  // TODO: For a rainy day- auto derive these using shapeless magic
+
+  implicit val FailedCancellationHasCommName =
     HasCommName.instance[FailedCancellationV2](_.cancellationRequested.commName)
-  implicit val failedHasCommName            = HasCommName.instance[FailedV2](_.metadata.commManifest.name)
-  implicit val orchStartedHasCommName       = HasCommName.instance[OrchestrationStartedV2](_.metadata.commManifest.name)
-  implicit val cancelledHasCommName         = HasCommName.instance[CancelledV2](_.cancellationRequested.commName)
+  implicit val FailedHasCommName            = HasCommName.instance[FailedV2](_.metadata.commManifest.name)
+  implicit val OrchStartedHasCommName       = HasCommName.instance[OrchestrationStartedV2](_.metadata.commManifest.name)
+  implicit val CancelledHasCommName         = HasCommName.instance[CancelledV2](_.cancellationRequested.commName)
   implicit val OrchestratedEmailHasCommName = HasCommName.instance[OrchestratedEmailV3](_.metadata.commManifest.name)
   implicit val OrchestratedSMSHasCommName   = HasCommName.instance[OrchestratedSMSV2](_.metadata.commManifest.name)
+  implicit val SMSProgressedHasCommName     = HasCommName.instance[SMSProgressedV2](_.metadata.commManifest.name)
+  implicit val LinkClickedHasCommName       = HasCommName.instance[LinkClickedV2](_.metadata.commManifest.name)
+  implicit val EmailProgressedHasCommName   = HasCommName.instance[EmailProgressedV2](_.metadata.commManifest.name)
+
 }
